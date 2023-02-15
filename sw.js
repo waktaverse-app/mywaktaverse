f12ffff517f5856dde8ffb6b5527e1e4c1251253
NeuroWhAI <tlsehdgus0212@gmail.com>
2023-02-15 19:19:51 +0900

    Setup PWA

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE = 'cache-v1';
+
+const PRECACHE_URLS = [
+  'index.html',
+  'favicon.ico',
+  'styles/global.css',
+  'fonts/fonts.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  const currentCaches = [CACHE];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin) && self.location.origin.indexOf('//localhost') < 0) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
